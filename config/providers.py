ae60f031bf397b83b6de8769d745130c1a17f0bf
jli5 <jeffli5191@gmail.com>
2021-12-06 17:33:07 -0500

    cors

--- config/providers.py
+++ config/providers.py
@@ -2,6 +2,7 @@
 
 from masonite.providers import (
     AppProvider,
+    CorsProvider,
     RequestHelpersProvider,
     AuthenticationProvider,
     BroadcastProvider,
@@ -15,8 +16,7 @@ from masonite.providers import (
     StatusCodeProvider,
     UploadProvider,
     ViewProvider,
-    WhitenoiseProvider,
-    CorsProvider
+    WhitenoiseProvider
 )
 from masonite.logging.providers import LoggingProvider
 from masonite.validation.providers import ValidationProvider
